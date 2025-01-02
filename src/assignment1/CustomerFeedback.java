6d0bc48fe8b0cdc62209c9c92441fea30ced2aab
Shiffali914 <Shiffalibansal914@gmail.com>
2025-01-01 23:26:42 -0500

    first commit

---
+++ src/assignment1/CustomerFeedback.java
@@ -0,0 +1,26 @@
+package assignment1;
+import java.util.Scanner;
+public class CustomerFeedback {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String[] reviews = new String[5];
+        System.out.println("enter the reviews");
+        for (int i = 0; i < reviews.length; i++) {
+            reviews[i] = sc.nextLine();
+        }
+        boolean found = false;
+        System.out.println("Enter word to search: ");
+        String word = sc.next();
+        for (int i = 0; i < reviews.length; i++) {
+            if (reviews[i].contains(word)) {
+                found = true;
+                System.out.println(reviews[i]);
+
+            }
+
+        }
+
+    }
+    }
+
